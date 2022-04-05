3da76728b9d3fba91a5f5cbc5275c843c8c0fea2
Blenda Öhman <blenda.ohman@gmail.com>
2022-04-05 15:40:47 +0200

    App working

---
+++ app/src/main/java/com/example/hellosensor/Accelerometer.java
@@ -0,0 +1,49 @@
+package com.example.hellosensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Accelerometer extends AppCompatActivity implements SensorEventListener {
+    SensorManager mSensorManager;
+    Sensor mAccelerometer;
+    private TextView outputX, outputY, outputZ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        outputX = findViewById(R.id.AccX);
+        outputY = findViewById(R.id.AccY);
+        outputZ = findViewById(R.id.AccZ);
+
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            outputX.setText(String.valueOf(event.values[0]));
+            outputY.setText(String.valueOf(event.values[1]));
+            outputZ.setText(String.valueOf(event.values[2]));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
---
+++ app/src/main/java/com/example/hellosensor/Compass.java
@@ -0,0 +1,118 @@
+package com.example.hellosensor;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.hardware.SensorEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+
+public class Compass extends AppCompatActivity implements SensorEventListener {
+    private ImageView compassImage;
+
+    //private float DegreeStart = 0f;
+    TextView compassText;
+    private SensorManager mSensorManager;
+    private Sensor mRotationV, mAccelerometer, mMagnetometer;
+    boolean haveSensor = false, haveSensor2 = false;
+
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    int mAzimuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        compassImage = findViewById(R.id.compassImage); //ImageView that will be displaying image of compass
+        compassImage.setImageResource(R.drawable.img);
+        compassText = findViewById(R.id.tvHeading); //TextView will be displaying the degree
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE); //Lets us access the sensors
+
+        start();
+    }
+
+    private void start() {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) {
+
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+            haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+
+        }
+        else{
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+
+    public void stop() {
+        if(haveSensor && haveSensor2){
+            mSensorManager.unregisterListener(this,mAccelerometer);
+            mSensorManager.unregisterListener(this,mMagnetometer);
+        }
+        else{
+            if(haveSensor)
+                mSensorManager.unregisterListener(this,mRotationV);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stop();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        start();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        mAzimuth = Math.round(mAzimuth);
+        compassImage.setRotation(-mAzimuth);
+
+        compassText.setText(mAzimuth + "° " );
+    }
+
+    //Not using
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
